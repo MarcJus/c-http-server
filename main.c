3c5a98d1625903d0c784066af878ceb35df57e92
Marc <marc@MacBook-Air-de-Marc.local>
2024-02-15 15:04:41 +0100

    Suppression de la macro check()
    C'est vraiment moche

--- main.c
+++ main.c
@@ -5,15 +5,18 @@
 
 #include "server.h"
 
-#define check(value, action) if(value < 0){ action }
-
 int main(int argc, char const *argv[]){
 
 	int ret;
 	printf("Création du serveur HTTP en cours...\n");
 
 	ret = create_server(80);
-	check(ret, perror("Erreur lors de la création du serveur"); return EXIT_FAILURE;)
+	if(ret < 0){
+		perror("Erreur lors de la création du serveur");
+		return EXIT_FAILURE;
+	}
+
+	
 	
 	return 0;
 }
