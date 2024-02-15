cc96aafa5471e43dcd48cfcd2baf62635497ff13
Marc <marc@MacBook-Air-de-Marc.local>
2024-02-15 17:31:27 +0100

    Alloue un buffer de 1024 charactères
    En attendant d'avoir un système qui lise l'integralité de la requête

--- http.c
+++ http.c
@@ -8,19 +8,20 @@
 
 int read_http_request(int client_socket){
 	while(1){
-		char *buffer = malloc(256);
+		char *buffer = malloc(1024);
 		if(buffer == NULL){
 			return -1;
 		}
-		ssize_t bytes_read = recv(client_socket, buffer, 255, 0);
+		bzero(buffer, 1024);
+		ssize_t bytes_read = recv(client_socket, buffer, 1023, 0);
 		if(bytes_read < 0){
 			return -1;
 		} else if (bytes_read == 0){
 			break;
 		} else {
-			buffer[255] = 0;
 			printf("%s", buffer);
 		}
+
 	}
 
 	return 0;
