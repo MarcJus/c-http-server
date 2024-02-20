f4820c788428cff7652216fe6deb19e97460abb1
MarcJus <jusseaumemarc@gmail.com>
2024-02-20 13:32:15 +0100

    Ouverture du file descriptor

--- file.c
+++ file.c
@@ -2,34 +2,25 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #include "file.h"
 
 #define INDEX_HTML "index.html"
 
 int open_file(const char *file_name){
-	int file_fd;
+	int fd;
 
 	if(strcmp(file_name, "")){ // Si la requête est /
 		return open("index.html", O_RDONLY);
 	}
 
-	file_fd = open(file_name, O_RDONLY);
-	if(file_fd < 0){
-		if(errno == ENOENT){
-			size_t new_path_len = sizeof(INDEX_HTML) + strlen(file_name);
-			char *new_path = malloc(sizeof(INDEX_HTML) + strlen(file_name));
-			if(new_path == NULL){
-				return -1;
-			} else {
-				bzero(new_path, new_path_len);
-				strncpy(new_path, file_name, strlen(file_name));
-				strncat(new_path, INDEX_HTML, new_path_len);
-
-				file_fd = open(new_path, O_RDONLY);
-			}
-		}
+	fd = open(file_name, O_RDONLY | O_NONBLOCK);
+	if(fd < 0){
+		return fd;
 	}
+	
 
-	return file_fd;
+	return fd;
 }
