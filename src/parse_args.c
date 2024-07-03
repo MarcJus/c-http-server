09e11323615a280f97470b3deb6ff117f307fc19
MarcJus <marcjuss@proton.me>
2024-07-03 15:57:39 +0200

    Gestion de l'option -r

--- src/parse_args.c
+++ src/parse_args.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <string.h>
 
 #include "parse_args.h"
 
@@ -18,8 +20,29 @@ int parse_args(int argc, char const* argv[]){
 		{ "verbose",	no_argument, 		NULL, 'v' },
 		{ NULL, 		no_argument, 		NULL, '\0'}
 	};
+	int root_flag = 0;
 
 	while((optval = getopt_long(argc, argv, short_options, long_options, NULL)) != EOF){
+
+		switch(optval){
+			case 'r':
+				size_t root_len = strlen(optarg); 
+				char *root = malloc(root_len) + 1; // +1 pour le \0
+				if(root == NULL){
+					return EXIT_FAILURE;
+				}
+
+				strncpy(root, optarg, root_len);
+				root[root_len] = '\0';
+				printf("root : %s\n", root);
+				root_flag = 1;
+
+				settings[SETTING_ROOT].value = root;
+				break;
+
+			default:
+				break;
+		}
 
 	}
 
